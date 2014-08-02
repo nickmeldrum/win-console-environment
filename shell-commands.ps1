84a39b0318275176bea15895f2aa75657ea438db
Nick Meldrum <nick@nickmeldrum.com>
2014-08-02 10:30:42 +0100

    fixed up list-colors and clipboard commands

--- shell-commands.ps1
+++ shell-commands.ps1
@@ -11,7 +11,43 @@ function WriteUnderlined-Host {
 WriteUnderlined-Host "Shell Commands and useful commands to remember:"
 
 function List-Colors {
-    write-host "Black | DarkBlue | DarkGreen | DarkCyan | DarkRed | DarkMagenta | DarkYellow | Gray | DarkGray | Blue | Green | Cyan | Red | Magenta | Yellow | White"
+    WriteUnderlined-Host "Powershell console colors:"
+
+    write-host " | White: " -nonewline
+    write-host "White" -ForegroundColor White -nonewline
+    write-host " | Gray: " -nonewline
+    write-host "Gray" -ForegroundColor Gray -nonewline
+    write-host " | DarkGray: " -nonewline
+    write-host "DarkGray" -ForegroundColor DarkGray -nonewline
+    write-host "| Black: " -nonewline
+    write-host "Black" -ForegroundColor Black -nonewline
+
+    write-host " | Blue: " -nonewline
+    write-host "Blue" -ForegroundColor Blue -nonewline
+    write-host " | DarkBlue: " -nonewline
+    write-host "DarkBlue" -ForegroundColor DarkBlue -nonewline
+    write-host " | Cyan: " -nonewline
+    write-host "Cyan" -ForegroundColor Cyan -nonewline
+    write-host " | DarkCyan: " -nonewline
+    write-host "DarkCyan" -ForegroundColor DarkCyan -nonewline
+    write-host " | Green: " -nonewline
+    write-host "Green" -ForegroundColor Green -nonewline
+    write-host " | DarkGreen: " -nonewline
+    write-host "DarkGreen" -ForegroundColor DarkGreen -nonewline
+    write-host " | DarkYellow: " -nonewline
+    write-host "DarkYellow" -ForegroundColor DarkYellow -nonewline
+    write-host " | Yellow: " -nonewline
+    write-host "Yellow" -ForegroundColor Yellow -nonewline
+    write-host " | Red: " -nonewline
+    write-host "Red" -ForegroundColor Red -nonewline
+    write-host " | DarkRed: " -nonewline
+    write-host "DarkRed" -ForegroundColor DarkRed -nonewline
+    write-host " | Magenta: " -nonewline
+    write-host "Magenta" -ForegroundColor Magenta -nonewline
+    write-host " | DarkMagenta: " -nonewline
+    write-host "DarkMagenta" -ForegroundColor DarkMagenta -nonewline
+
+    write-host " |"
 }
 
 # For when you've cloned into a directory with the same name as parent directory
@@ -31,8 +67,14 @@ function Create-TextFile {
 
 Set-Alias copycon Create-TextFile
 
+function Copy-CurDirToClipboard {
+    $pwd.Path | clip
+}
+
+Set-Alias copydir Copy-CurDirToClipboard
+
 write-host "copycon x y                   | Create a text file with the filename x and text y"
 write-host "Move-AllItemsToParentDir      | Does what it says on the tin (and deletes current dir!)"
-write-host "`"x | clip`"                    | copy to clipboard"
-write-host "`"cd | clip`"                   | copy current dir to clipboard"
+write-host "x | clip                      | copy to clipboard"
+write-host "copydir                       | copy current dir to clipboard"
 write-host "pushd and popd                | push and pop directories from stack"
