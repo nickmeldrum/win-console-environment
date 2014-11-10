3dbac7a9bc146d895c3bd4d97841dc303aabdf4c
Nick Meldrum <nick@nickmeldrum.com>
2014-11-10 15:00:12 +0000

    Update copy-of-profile.ps1

--- copy-of-profile.ps1
+++ copy-of-profile.ps1
@@ -5,7 +5,7 @@ $githubToken = "mysecrettokenhere!"
 
 Import-Module "PowerTab" -ArgumentList "C:\Users\Nick\Documents\WindowsPowerShell\PowerTabConfig.xml"
 
-. 'C:\Users\Nick\src\others\posh-git\posh-git\profile.example.ps1'
+. 'C:\users\nick\src\current\powershell-productivity-scripts\posh-git\profile.example.ps1'
 . 'C:\users\nick\src\current\powershell-productivity-scripts\shell-commands.ps1'
 . 'C:\users\nick\src\current\powershell-productivity-scripts\git-commands.ps1'
 . 'C:\users\nick\src\current\powershell-productivity-scripts\dotnet-commands.ps1'
