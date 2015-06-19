510a39ff56d53800df15b83be14c03139aaa01a5
Nick Meldrum <nick@nickmeldrum.com>
2015-06-19 11:11:05 +0100

    speeds up profile loading

--- dotnet-commands.ps1
+++ dotnet-commands.ps1
@@ -1,24 +1,28 @@
-#Set environment variables for Visual Studio Command Prompt
-pushd 'c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC'
+function Set-DotNetEnvironment {
+    #Set environment variables for Visual Studio Command Prompt
+    pushd 'c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC'
 
-cmd /c "vcvarsall.bat&set" |
-foreach {
-  if ($_ -match "=") {
-    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-  }
+    cmd /c "vcvarsall.bat&set" |
+    foreach {
+      if ($_ -match "=") {
+        $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+      }
+    }
+    popd
+    write-host "dotnet environment variables set, msbuild tools path: " -ForegroundColor Yellow -NoNewLine
+
+    $msbuildPathData = reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0" /v MSBuildToolsPath
+    $msbuildPath = (($msbuildpathData[2] -split "\s+")[3]).Trim()
+    write-host $msbuildPath -ForegroundColor magenta
 }
-popd
-write-host "dotnet environment variables set, msbuild tools path: " -ForegroundColor Yellow -NoNewLine
-
-$msbuildPathData = reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0" /v MSBuildToolsPath
-$msbuildPath = (($msbuildpathData[2] -split "\s+")[3]).Trim()
-write-host $msbuildPath -ForegroundColor magenta
 
 Set-Alias vs "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
 Set-Alias msbuild ($msbuildPath + "\msbuild.exe")
+Set-Alias dotnetsetup Set-DotNetEnvironment
 
 function Echo-DotnetCommands {
     Write-Title "Dotnet commands:"
+    write-host "dotnetsetup                   | setup dotnet environment variables and msbuild tools path"
     write-host "vs x                          | open solution x in visual studio"
     write-host "msbuild x                     | run msbuild with args x"
 }
