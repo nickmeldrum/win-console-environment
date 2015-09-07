5f6658d4f433ddee7be26128dfe1c65a70f330b8
Nick Meldrum <nick@nickmeldrum.com>
2015-09-07 07:47:09 +0100

    moves all local config to separate file

--- copy-of-profile.ps1
+++ copy-of-profile.ps1
@@ -1,34 +1,46 @@
-# Fix these vars per machine:
-$userRoot = "C:\Users\nick.meldrum"
-$envRepo = "D:\env"
-$scribestarRepo = "D:\prod"
+$ErrorActionPreference = "Stop"
 
-$githubToken = (& "$envrepo\3rdparty\credman.ps1" -getcred -target 'nicks:githubToken').password
+# Setup a file in your home directory called localconfig.json like this:
+# {
+#     "userRoot": "C:/Users/nick.meldrum",
+#     "envRepo": "D:/env",
+#     "scribestarRepo": "D:/prod"
+# }
 
-$githubUsername = (& "$envrepo\3rdparty\credman.ps1" -getcred -target 'nicks:githubPassword').username
-$githubPassword = (& "$envrepo\3rdparty\credman.ps1" -getcred -target 'nicks:githubPassword').password
+if (-not (test-path ~/localconfig.json)) {
+    throw "localconfig.json file not found!"
+}
 
-$bitbucketUsername = (& "$envrepo\3rdparty\credman.ps1" -getcred -target 'nicks:bitbucketToken').username
-$bitbucketToken = (& "$envrepo\3rdparty\credman.ps1" -getcred -target 'nicks:bitbucketToken').password
+$localConfig = get-content ~/localconfig.json -raw | convertfrom-json
 
-$dnsimpleEmail = (& "$envrepo\3rdparty\credman.ps1" -getcred -target 'nicks:dnsimpleToken').username
-$dnsimpleToken = (& "$envrepo\3rdparty\credman.ps1" -getcred -target 'nicks:dnsimpleToken').password
+import-module "$($localConfig.envrepo)\passwordvault.psm1" -force
 
-$env:PSModulePath = $env:PSModulePath + ";$envRepo\3rdparty"
+$githubToken = (Get-PasswordVaultCredentials 'nicks:githubToken').password
+
+$githubUsername = (Get-PasswordVaultCredentials 'nicks:githubPassword').username
+$githubPassword = (Get-PasswordVaultCredentials 'nicks:githubPassword').password
+
+$bitbucketUsername = (Get-PasswordVaultCredentials 'nicks:bitbucketToken').username
+$bitbucketToken = (Get-PasswordVaultCredentials 'nicks:bitbucketToken').password
+
+$dnsimpleEmail = (Get-PasswordVaultCredentials 'nicks:dnsimpleToken').username
+$dnsimpleToken = (Get-PasswordVaultCredentials 'nicks:dnsimpleToken').password
+
+$env:PSModulePath = $env:PSModulePath + ";$($localConfig.envrepo)\3rdparty"
 # Import-Module "PowerTab" -ArgumentList "$userRoot\Documents\WindowsPowerShell\PowerTabConfig.xml"
 echo "setting up posh-git..."
-. "$envRepo\3rdparty\posh-git\profile.example.ps1"
+. "$($localConfig.envrepo)\3rdparty\posh-git\profile.example.ps1"
 echo "setting up shell commands..."
-. "$envRepo\shell-commands.ps1"
+. "$($localConfig.envrepo)\shell-commands.ps1"
 echo "setting up system commands..."
-. "$envRepo\system-commands.ps1"
+. "$($localConfig.envrepo)\system-commands.ps1"
 echo "setting up git commands..."
-. "$envRepo\git-commands.ps1"
+. "$($localConfig.envrepo)\git-commands.ps1"
 echo "setting up dotnet commands..."
-. "$envRepo\dotnet-commands.ps1"
+. "$($localConfig.envrepo)\dotnet-commands.ps1"
 echo "setting up scribestar commands..."
-. "$envRepo\scribestar-commands.ps1"
+. "$($localConfig.envrepo)\scribestar-commands.ps1"
 echo "setting up launch commands..."
-. "$envRepo\launch-commands.ps1"
-ep # this command exists in launch-commands.ps1
+. "$($localConfig.envrepo)\launch-commands.ps1"
+# ep # this command exists in launch-commands.ps1
 
