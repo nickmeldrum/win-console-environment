482621e221baa366ee52a085016df25f30bb72f8
Nick Meldrum <nick@nickmeldrum.com>
2015-09-25 23:42:56 +0100

    updates to install script

--- install.ps1
+++ install.ps1
@@ -1,14 +1,8 @@
-# 3rd party application installs
-choco install conemu -y
-choco install autohotkey -y
-choco install git -y -params '"/GitAndUnixToolsOnPath"' --force
-choco install vim -y --force
-choco install passwordsafe -y
-choco install dropbox -y
-choco install nodejs -y
-choco install sublimetext2 -y
-
+#local env
 mkdir c:\work
+
+#git
+choco install git -y -params '"/GitAndUnixToolsOnPath"' --force
 
 # Install my fork of git credential store
 cd c:\work
@@ -21,15 +15,15 @@ msbuild
 cd c:\work
 git clone https://nickmeldrum@github.com/nickmeldrum/win-console-environment.git env
 cd env
-if (test-path $profile) {
-	del $profile -force
-}
-new-item $profile -force -type file
-copy copy-of-profile.ps1 $profile -force
 copy .gitconfig ~/.gitconfig
-copy .npmrc ~/.npmrc
-copy .vimrc ~/.vimrc
 copy localconfig.json ~/localconfig.json
+
+# nodejs
+choco install nodejs -y
+copy .npmrc ~/.npmrc
+
+# autohotkey
+choco install autohotkey -y
 copy autohotkey.ahk ~/Documents/autohotkey.ahk
 
 # Setup autohotkey to run and always run on logon
@@ -39,17 +33,35 @@ $trigger = New-ScheduledTaskTrigger -AtLogOn
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "AutoHotkey"
 
 # Setup powershell environment - profile/ posh git etc. not being picked up?
+if (test-path $profile) {
+	del $profile -force
+}
+new-item $profile -force -type file
+copy copy-of-profile.ps1 $profile -force
+cd 3rdparty
+git clone https://github.com/dahlbyk/posh-git.git
+cd ..
 
 # Setup vim
-# check .vimrc is working?
-# clone vundle
-# do plugin install
+ren "C:\Program Files\Git\usr\bin\vim.exe" "C:\Program Files\Git\usr\bin\vim.exe.bak"
+choco install vim -y --force
+copy .vimrc ~/.vimrc
+mkdir ~/.vim/bundle
+cd ~/.vim/bundle
+git clone https://github.com/gmarik/Vundle.vim.git
+cd c:\work\env
+vim +PluginInstall +qall
 
 # Setup conemu
+choco install conemu -y
 # import conemu settings
 # create cmd/ vim/ scribestar and npm shortcuts with icons on the taskbar
 
 # Setup sublime text license
+choco install sublimetext2 -y
 
 # Setup diff tool
 
+# 3rd party application installs
+choco install passwordsafe -y
+choco install dropbox -y
