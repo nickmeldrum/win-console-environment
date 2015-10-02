2a453d96f0e500308388fa0ac113bb62c6328aa3
Nick Meldrum <nick@nickmeldrum.com>
2015-10-02 08:11:47 +0100

    adds local updating script

---
+++ update-from-local.ps1
@@ -0,0 +1,31 @@
+param([string]$commitmsg)
+
+function copyfileifunaltered {
+    param ($filename, $localLocation)
+
+
+    if (-not [string]::isnullorwhitespace((git status --porcelain | grep "$filename"))) {
+        return "$filename altered in local git, please push local changes first. not copying over..."
+    }
+    else {
+        echo "copying $filename to $locallocation..."
+        copy $localLocation "./$filename" -force
+    }
+}
+
+copyfileifunaltered ".vimrc" "~/.vimrc"
+copyfileifunaltered ".npmrc" "~/.npmrc"
+copyfileifunaltered ".gitconfig" "~/.gitconfig"
+copyfileifunaltered "localconfig.json" "~/localconfig.json"
+copyfileifunaltered "autohotkey.ahk" "~/Documents/autohotkey.ahk"
+
+if (-not [string]::isnullorwhitespace($commitmsg)) {
+    echo "commit msg passed in, attempting to commit and push..."
+    git add .
+    git commit -v -m $commitmsg
+    git push
+}
+else {
+    echo "no commit msg passed in, please manually commit and push..."
+    git status
+}
