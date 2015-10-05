6cb9ffbe68d102ac7b7856fced6b30a9efcb6f01
Nick Meldrum <nick@nickmeldrum.com>
2015-10-05 13:43:01 +0100

    adds choco install check

--- run.cmd
+++ run.cmd
@@ -1,2 +1,13 @@
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
-@powershell -NoProfile -ExecutionPolicy Bypass -Command ".\install.ps1"
+@echo off
+choco > nul 2> nul
+if %errorlevel% EQU 1 (
+    echo "choco already installed"
+) else if %errorlevel% EQU 9009 (
+    echo "choco not installed, installing..."
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" 
+    SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+) else (
+    echo "error checking choco install: %errorlevel%"
+)
+echo "running powershell install script..."
+powershell -NoProfile -ExecutionPolicy Bypass -Command ".\install.ps1"
