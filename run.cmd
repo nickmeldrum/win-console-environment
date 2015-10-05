16cd55b8f9b25f2623bb2b4891c9fa376b01b7a0
Nick Meldrum <nick@nickmeldrum.com>
2015-10-05 16:33:23 +0100

    adds idempotency to choco install and gets own ps isntall script from github

--- run.cmd
+++ run.cmd
@@ -10,4 +10,4 @@ if %errorlevel% EQU 1 (
     echo "error checking choco install: %errorlevel%"
 )
 echo "running powershell install script..."
-powershell -NoProfile -ExecutionPolicy Bypass -Command ".\install.ps1"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/nickmeldrum/win-console-environment/master/install.ps1'))"
