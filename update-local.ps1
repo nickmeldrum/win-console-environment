4faebd9831db32df64a7f8f24a977953ad664820
Nick Meldrum <nick@nickmeldrum.com>
2016-03-17 10:07:45 +0000

    chaser

--- update-local.ps1
+++ update-local.ps1
@@ -2,13 +2,11 @@ function copyfileifunaltered {
     param ($filename, $localLocation)
 
     echo "overwriting $filename to $locallocation..."
-    copy $localLocation "./$filename" -force
+    copy "./$localLocation" $filename -force
 }
 
-copyfileifunaltered ".vimrc" "~/.vimrc"
-copyfileifunaltered ".npmrc" "~/.npmrc"
-#copyfileifunaltered ".gitconfig" "~/.gitconfig"
-#copyfileifunaltered "localconfig.json" "~/localconfig.json"
-copyfileifunaltered "autohotkey.ahk" "~/Documents/autohotkey.ahk"
-copyfileifunaltered "snippets/javascript.snippets" "~/vimfiles/snippets/javascript.snippets"
+copyfileifunaltered "~/.vimrc" ".vimrc"
+copyfileifunaltered "~/.npmrc" ".npmrc"
+copyfileifunaltered "~/Documents/autohotkey.ahk" "autohotkey.ahk"
+copyfileifunaltered "~/vimfiles/snippets/javascript.snippets" "snippets/javascript.snippets"
 
